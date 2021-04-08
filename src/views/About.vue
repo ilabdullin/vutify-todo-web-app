3513c79ab51e2243868aea843616349faa4fc891
ilabdullin <ilnurabdullin578@gmail.com>
2021-04-08 11:52:36 +0500

    first version

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,35 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+    <div class="about pa-6">
+        <h1>About Vuetify Todo</h1>
+
+        <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae,
+            quam ratione ipsa eos laborum unde officia rerum nobis impedit
+            molestias minus aliquid quibusdam numquam id eum dolore quos sint
+            est?Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Molestiae, quam ratione ipsa eos laborum unde officia rerum nobis
+            impedit molestias minus aliquid quibusdam numquam id eum dolore quos
+            sint est?
+        </p>
+
+        <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae,
+            quam ratione ipsa eos laborum unde officia rerum nobis impedit
+            molestias minus aliquid quibusdam numquam id eum dolore quos sint
+            est?Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Molestiae, quam ratione ipsa eos laborum unde officia rerum nobis
+            impedit molestias minus aliquid quibusdam numquam id eum dolore quos
+            sint est?
+        </p>
+
+        <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae,
+            quam ratione ipsa eos laborum unde officia rerum nobis impedit
+            molestias minus aliquid quibusdam numquam id eum dolore quos sint
+            est?Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Molestiae, quam ratione ipsa eos laborum unde officia rerum nobis
+            impedit molestias minus aliquid quibusdam numquam id eum dolore quos
+            sint est?
+        </p>
+    </div>
 </template>
